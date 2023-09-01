5b4566f8e986e603f2b80e3a04a0bf727dcb652c
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-09-01 20:59:12 +1000

    getting file name and extension in screenshot cleanup

--- screenshot-cleanup/src/main.rs
+++ screenshot-cleanup/src/main.rs
@@ -1,16 +1,38 @@
-use std::env;
-use std::{fs, path::Path};
+use std::fs::metadata;
+use std::path::{Path, PathBuf};
+
+fn print_type<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
 
 fn main() {
     let path = Path::new("/Users/LJPurcell/Desktop/");
-    let cd_result = env::set_current_dir(path);
-    let file_paths = match cd_result {
-        Ok(cd_result) => fs::read_dir(path),
-        Err(error) => panic!("Couldn't change to Desktop using path: {}", path.display()),
+    let cd_result = std::env::set_current_dir(path);
+
+    let entries_result = match cd_result {
+        Ok(_) => std::fs::read_dir("."),
+        Err(_) => panic!("Couldn't change directory using path: {}", path.display()),
     };
 
-    for path in file_paths.unwrap() {
-        let fp = path.unwrap();
-        println!("{}", fp.path().display());
+    let entries = match entries_result {
+        Ok(entries) => entries,
+        Err(_) => panic!("Couldn't read current directory at: {}", path.display()),
+    };
+
+    for entry in entries {
+        let file = entry.unwrap().path();
+
+        if file.is_file() {
+            if file.extension().is_some() {
+                let extension = file.extension().unwrap();
+
+                if extension == "png" {
+                    println!("Found the png");
+                }
+
+                let file_name = file.to_str();
+                println!("{:?}", file_name);
+            };
+        }
     }
 }
