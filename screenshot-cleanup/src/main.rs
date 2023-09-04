3950b197c735223a877c3042db2feaeb453a8a65
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-09-04 16:19:31 +1000

    finished cleanup script, built, and set cronjob

--- screenshot-cleanup/src/main.rs
+++ screenshot-cleanup/src/main.rs
@@ -1,37 +1,41 @@
-use std::fs::metadata;
-use std::path::{Path, PathBuf};
-
-fn print_type<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>());
-}
+use std::{fs, path::Path};
 
 fn main() {
     let path = Path::new("/Users/LJPurcell/Desktop/");
     let cd_result = std::env::set_current_dir(path);
 
-    let entries_result = match cd_result {
+    let read_dir_result = match cd_result {
         Ok(_) => std::fs::read_dir("."),
         Err(_) => panic!("Couldn't change directory using path: {}", path.display()),
     };
 
-    let entries = match entries_result {
+    let entries = match read_dir_result {
         Ok(entries) => entries,
         Err(_) => panic!("Couldn't read current directory at: {}", path.display()),
     };
 
     for entry in entries {
-        let file = entry.unwrap().path();
+        let file_path = match entry {
+            Ok(entry) => entry.path(),
+            Err(_) => panic!("Couldn't get entry at: {:?}", entry),
+        };
 
-        if file.is_file() {
-            if file.extension().is_some() {
-                let extension = file.extension().unwrap();
+        if file_path.is_file() {
+            if file_path.extension().is_some() {
+                let extension = file_path
+                    .extension()
+                    .unwrap_or_else(|| panic!("Could't get extension for file: {:?}", file_path));
 
-                if extension == "png" {
-                    println!("Found the png");
+                let file_name = file_path.to_str().unwrap_or_else(|| {
+                    panic!(
+                        "Couldn't convert file path to string for file {:?}",
+                        file_path
+                    )
+                });
+
+                if extension == "png" && file_name.starts_with("./Screenshot 2023-") {
+                    fs::remove_file(file_name).unwrap_or_else(|file_name| panic!("Error when removing file: {:?}", file_name));
                 }
-
-                let file_name = file.to_str();
-                println!("{:?}", file_name);
             };
         }
     }
